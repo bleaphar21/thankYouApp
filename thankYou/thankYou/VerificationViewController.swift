623c57b664b0c7b8219e4fd02380484aee4d7d63
Brantley Leaphart <Brantleylea@gmail.com>
2019-08-08 15:32:02 -0700

    The basic set up for the thank you app

---
+++ thankYou/thankYou/VerificationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VerificationViewController.swift
+//  thankYou
+//
+//  Created by Brantley Leaphart  on 8/8/19.
+//  Copyright © 2019 Brantley Leaphart . All rights reserved.
+//
+
+import UIKit
+
+class VerificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
